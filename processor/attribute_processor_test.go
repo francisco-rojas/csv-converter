4d65e5f227673d2d8634c304d48a82cba93d1434
Francisco Rojas <josefcorojas@gmail.com>
2019-12-14 15:06:05 +0100

    Only test exported functions for attribute_processor.go

--- processor/attribute_processor_test.go
+++ processor/attribute_processor_test.go
@@ -7,266 +7,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAttribute_converterFor(t *testing.T) {
-	type fields struct {
-		Row        []string
-		Options    map[string]string
-		Mappings   map[interface{}]interface{}
-		HasHeaders bool
-		Headers    []string
-	}
-	type args struct {
-		c string
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   converter.Converter
-	}{
-		{
-			name: "when string converter provided",
-			fields: fields{
-				Row:     []string{"John", "Doe"},
-				Options: map[string]string{},
-				Mappings: map[interface{}]interface{}{
-					"header":     1,
-					"converters": map[interface{}]interface{}{},
-				},
-			},
-			args: args{"string"},
-			want: &converter.String{RawData: "Doe", Options: map[string]string{}},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			ap := &Attribute{
-				Row:        tt.fields.Row,
-				Options:    tt.fields.Options,
-				Mappings:   tt.fields.Mappings,
-				HasHeaders: tt.fields.HasHeaders,
-				Headers:    tt.fields.Headers,
-			}
-
-			v := ap.converterFor(tt.args.c)
-			assert.Equal(t, tt.want, v)
-		})
-	}
-}
-
-func TestAttribute_converterForErrors(t *testing.T) {
-	type fields struct {
-		Row        []string
-		Options    map[string]string
-		Mappings   map[interface{}]interface{}
-		HasHeaders bool
-		Headers    []string
-	}
-	type args struct {
-		c string
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		{
-			name: "when invalid converter provided",
-			fields: fields{
-				Row:     []string{"John", "Doe"},
-				Options: map[string]string{},
-				Mappings: map[interface{}]interface{}{
-					"header":     1,
-					"converters": map[interface{}]interface{}{},
-				},
-			},
-			args: args{"invalid"},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			ap := &Attribute{
-				Row:        tt.fields.Row,
-				Options:    tt.fields.Options,
-				Mappings:   tt.fields.Mappings,
-				HasHeaders: tt.fields.HasHeaders,
-				Headers:    tt.fields.Headers,
-			}
-
-			assert.PanicsWithValue(t, converter.ErrInvalidConverter{Name: tt.args.c}, func() { ap.converterFor(tt.args.c) })
-		})
-	}
-}
-
-func TestAttribute_data(t *testing.T) {
-	type fields struct {
-		Row        []string
-		Options    map[string]string
-		Mappings   map[interface{}]interface{}
-		HasHeaders bool
-		Headers    []string
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   string
-	}{
-		{
-			name: "when no headers provided",
-			fields: fields{
-				Row:     []string{"John", "Doe"},
-				Options: map[string]string{},
-				Mappings: map[interface{}]interface{}{
-					"header": 0,
-					"converters": map[interface{}]interface{}{
-						"string": nil,
-					},
-				},
-			},
-			want: "John",
-		},
-		{
-			name: "when headers provided",
-			fields: fields{
-				Row:     []string{"John", "Doe"},
-				Options: map[string]string{},
-				Mappings: map[interface{}]interface{}{
-					"header": "Last Name",
-					"converters": map[interface{}]interface{}{
-						"string": nil,
-					},
-				},
-				HasHeaders: true,
-				Headers:    []string{"First Name", "Last Name"},
-			},
-			want: "Doe",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			ap := &Attribute{
-				Row:        tt.fields.Row,
-				Options:    tt.fields.Options,
-				Mappings:   tt.fields.Mappings,
-				HasHeaders: tt.fields.HasHeaders,
-				Headers:    tt.fields.Headers,
-			}
-
-			assert.Equal(t, tt.want, ap.data())
-		})
-	}
-}
-
-func TestAttribute_dataErrors(t *testing.T) {
-	type fields struct {
-		Row        []string
-		Options    map[string]string
-		Mappings   map[interface{}]interface{}
-		HasHeaders bool
-		Headers    []string
-	}
-	tests := []struct {
-		name   string
-		fields fields
-	}{
-		{
-			name: "when invalid header provided",
-			fields: fields{
-				Row:     []string{"John", "Doe"},
-				Options: map[string]string{},
-				Mappings: map[interface{}]interface{}{
-					"header": "Last Name",
-					"converters": map[interface{}]interface{}{
-						"string": nil,
-					},
-				},
-				HasHeaders: true,
-				Headers:    []string{"Name", "Surname"},
-			},
-		},
-		{
-			name: "when invalid header provided",
-			fields: fields{
-				Row:     []string{"John", "Doe"},
-				Options: map[string]string{},
-				Mappings: map[interface{}]interface{}{
-					"header": 5,
-					"converters": map[interface{}]interface{}{
-						"string": nil,
-					},
-				},
-				HasHeaders: false,
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			ap := &Attribute{
-				Row:        tt.fields.Row,
-				Options:    tt.fields.Options,
-				Mappings:   tt.fields.Mappings,
-				HasHeaders: tt.fields.HasHeaders,
-				Headers:    tt.fields.Headers,
-			}
-
-			assert.PanicsWithValue(t, converter.ErrInvalidColumn{Header: ap.Mappings["header"]}, func() { ap.data() })
-		})
-	}
-}
-
-func TestAttribute_invokeConverter(t *testing.T) {
-	type fields struct {
-		Row        []string
-		Options    map[string]string
-		Mappings   map[interface{}]interface{}
-		HasHeaders bool
-		Headers    []string
-	}
-	type args struct {
-		c    string
-		opts interface{}
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   interface{}
-	}{
-		{
-			name: "when string converter provided",
-			fields: fields{
-				Row:     []string{"John", "Doe"},
-				Options: map[string]string{},
-				Mappings: map[interface{}]interface{}{
-					"header": "Last Name",
-					"converters": map[interface{}]interface{}{
-						"string": nil,
-					},
-				},
-				HasHeaders: true,
-				Headers:    []string{"First Name", "Last Name"},
-			},
-			args: args{
-				c:    "string",
-				opts: map[interface{}]interface{}{},
-			},
-			want: "Doe",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			ap := &Attribute{
-				Row:        tt.fields.Row,
-				Options:    tt.fields.Options,
-				Mappings:   tt.fields.Mappings,
-				HasHeaders: tt.fields.HasHeaders,
-				Headers:    tt.fields.Headers,
-			}
-			assert.Equal(t, tt.want, ap.invokeConverter(tt.args.c, tt.args.opts))
-		})
-	}
-}
-
 func TestAttribute_ProcessAttribute(t *testing.T) {
 	type fields struct {
 		Row        []string
@@ -281,7 +21,7 @@ func TestAttribute_ProcessAttribute(t *testing.T) {
 		want   interface{}
 	}{
 		{
-			name: "processes a string attribute",
+			name: "when column header provided",
 			fields: fields{
 				Row:     []string{"John", "Doe"},
 				Options: map[string]string{},
@@ -295,6 +35,20 @@ func TestAttribute_ProcessAttribute(t *testing.T) {
 				Headers:    []string{"First Name", "Last Name"},
 			},
 			want: "Doe",
+		},
+		{
+			name: "when column index provided",
+			fields: fields{
+				Row:     []string{"John", "Doe"},
+				Options: map[string]string{},
+				Mappings: map[interface{}]interface{}{
+					"header": 0,
+					"converters": map[interface{}]interface{}{
+						"string": nil,
+					},
+				},
+			},
+			want: "John",
 		},
 		{
 			name: "trims spaces from a string attribute",
@@ -324,6 +78,81 @@ func TestAttribute_ProcessAttribute(t *testing.T) {
 			}
 
 			assert.Equal(t, tt.want, ap.ProcessAttribute())
+		})
+	}
+}
+
+func TestAttribute_ProcessAttributePanics(t *testing.T) {
+	type fields struct {
+		Row        []string
+		Options    map[string]string
+		Mappings   map[interface{}]interface{}
+		HasHeaders bool
+		Headers    []string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   interface{}
+	}{
+		{
+			name: "panics when invalid column header provided",
+			fields: fields{
+				Row:     []string{"John", "Doe"},
+				Options: map[string]string{},
+				Mappings: map[interface{}]interface{}{
+					"header": "Last Name",
+					"converters": map[interface{}]interface{}{
+						"string": nil,
+					},
+				},
+				HasHeaders: true,
+				Headers:    []string{"Name", "Surname"},
+			},
+			want: converter.ErrInvalidColumn{Header: "Last Name"},
+		},
+		{
+			name: "panics when invalid column index provided",
+			fields: fields{
+				Row:     []string{"John", "Doe"},
+				Options: map[string]string{},
+				Mappings: map[interface{}]interface{}{
+					"header": "5",
+					"converters": map[interface{}]interface{}{
+						"string": nil,
+					},
+				},
+			},
+			want: converter.ErrInvalidColumn{Header: "5"},
+		},
+		{
+			name: "panics when invalid converter provided",
+			fields: fields{
+				Row:     []string{"John", "Doe"},
+				Options: map[string]string{},
+				Mappings: map[interface{}]interface{}{
+					"header": "Last Name",
+					"converters": map[interface{}]interface{}{
+						"invalid": nil,
+					},
+				},
+				HasHeaders: true,
+				Headers:    []string{"First Name", "Last Name"},
+			},
+			want: converter.ErrInvalidConverter{Name: "invalid"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ap := &Attribute{
+				Row:        tt.fields.Row,
+				Options:    tt.fields.Options,
+				Mappings:   tt.fields.Mappings,
+				HasHeaders: tt.fields.HasHeaders,
+				Headers:    tt.fields.Headers,
+			}
+
+			assert.PanicsWithValue(t, tt.want, func() { ap.ProcessAttribute() })
 		})
 	}
 }
